909e5e55400e692adda2463ed15cfe0626a38ca6
cipher <cipherlofi04@gmail.com>
2023-03-09 18:18:22 +0300

    adding webhook functionality for the deployment

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { Bot } from "grammy";
+import { webhookCallback } from "grammy";
+import { express } from "express";
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -48,4 +50,18 @@ bot.on("message", msgHandler);
 // This will connect to the Telegram servers and wait for messages.
 
 // Start the bot.
-bot.start();
+// Start the server
+if (process.env.NODE_ENV === "production") {
+  // Use Webhooks for the production server
+  const app = express();
+  app.use(express.json());
+  app.use(webhookCallback(bot, "express"));
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Bot listening on port ${PORT}`);
+  });
+} else {
+  // Use Long Polling for development
+  bot.start();
+}
