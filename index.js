370b21bbda915b20d8df1e01c1777a016aae42a2
cipher <cipherlofi04@gmail.com>
2023-03-11 17:59:42 +0300

    adding changing to polling to host on vercel

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import { Bot } from "grammy";
-import { webhookCallback } from "grammy";
-import express from "express";
 import * as dotenv from "dotenv";
 dotenv.config();
-
-const allowedGroups = [-894217890];
+//-894217890 testgroup
+// -1001763257854 testSupergroup
+// -1001719821555 bit group
+const allowedGroups = [-1001719821555, -894217890, -1001763257854];
 const checkIfItBoss = async (ctx) => {
   if (ctx.chat.type == "private" && ctx.chat.id == process.env.BOSS) {
     await ctx.reply("hello boss what can i do for you!");
@@ -12,11 +12,11 @@ const checkIfItBoss = async (ctx) => {
     await ctx.reply(
       "you are not the boss so there is nothing i can do to you!"
     );
+    await bot.api.sendMessage(process.env.BOSS, ctx);
   }
 };
 const msgHandler = async (ctx) => {
-  if (ctx.chat.type == "group" && !allowedGroups.includes(ctx.chat.id)) {
-    await bot.api.sendMessage(process.env.BOSS, ctx);
+  if (ctx.chat.type !== "private" && !allowedGroups.includes(ctx.chat.id)) {
     await bot.api.leaveChat(ctx.chat.id);
   }
   if (ctx.message.document) {
@@ -51,17 +51,4 @@ bot.on("message", msgHandler);
 
 // Start the bot.
 // Start the server
-if (process.env.NODE_ENV === "production") {
-  // Use Webhooks for the production server
-  const app = express();
-  app.use(express.json());
-  app.use(webhookCallback(bot, "express"));
-
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Bot listening on port ${PORT}`);
-  });
-} else {
-  // Use Long Polling for development
-  bot.start();
-}
+bot.start();
